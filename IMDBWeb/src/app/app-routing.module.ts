852a65f637a5bc4134b22f94c0240cca6d2436d3
Irhad Mrndžić <irhad.mrndzic@gmail.com>
2021-11-07 22:35:28 +0100

    Redirection fix

--- IMDBWeb/src/app/app-routing.module.ts
+++ IMDBWeb/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: "home", pathMatch: "full", data: { type: "" } },
   { path: 'home', component: HomePageComponent, data: { type: "" } },
   { path: 'movies', component: HomePageComponent, data: { type: 0 } },
   { path: 'shows', component: HomePageComponent, data: { type: 1 } }
